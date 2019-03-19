b1d3267f405a782e033cfb84afb28c4844d0dd57
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-19 15:58:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,14 @@ WIN_COMBINATIONS = [
                       [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
                     ]
 def play(board)
-  input = gets
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congradulations #{winner(board)}"
+  elsif draw?(board)
+    puts "Cat's game!"
+  end
 end
 
 def display_board(board)
