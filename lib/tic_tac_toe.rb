5d5a04f3654ae116847042e76e762733e09b0177
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-19 16:03:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    puts "Congradulations #{winner(board)}"
+    puts "Congradulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's game!"
   end
