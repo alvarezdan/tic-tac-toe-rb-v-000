17649fa9646e8f1a48931a91bd68926ad4e4c9a1
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-19 15:30:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,14 +37,6 @@ def turn(board)
   end
 end
 
-def turn_count(board)
-  turns = 0
-  board.each do |play|
-    if play == "X" || play == "O"
-      turns +=1
-    end
-end
-
 def current_player(board)
   turn_count(board) % 2 == 0? "X" : "O"
 end
